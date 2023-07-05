961712114adac5f47a5f5fdf85e15a7f3228ed37
sorbon01 <sanginov.sorbon01@gmail.com>
2023-07-05 09:23:05 +0500

    mut & match

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,30 @@ fn add(num_one:i32, num_two:i32)-> i32 {
     return num_one+num_two;
 }
 fn main() {
-    let _foo = add(10, 5);
+    let mut total = add(10, 15);
+    let mut free_shipping = false;
+
+    if total > 50 {
+        println!("You qualify for free shipping!");
+        free_shipping = true;
+    }
+    else if total>20 {
+        println!("If you add more items, you can qualify for free shipping");
+    }
+    else {
+        println!("No free shipping");
+    }
+
+    total = match free_shipping {
+        true => total +0,
+        false => total +5
+    };
+
+    match total {
+        1 => println!("1"),
+        2 => println!("2"),
+        _ => println!("No match found")
+    }
+
+    println!("{:?}",total);
 }
